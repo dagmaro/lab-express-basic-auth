15ce2118ba89cc9515f383033522cb6766aedcf0
Dagmara Grabowska <dagmarag0@gmail.com>
2023-02-15 17:41:21 +0100

    iteration 1 solved

---
+++ routes/auth.routes.js
@@ -0,0 +1,54 @@
+const router = require("express").Router();
+
+const User = require("../models/User.model");
+const bcrypt = require("bcryptjs");
+
+// GET "/auth/signup"
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup.hbs");
+});
+
+// POST
+router.post("/signup", async (req, res, next) => {
+  const { username, password } = req.body;
+
+  // validación password
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+  if (passwordRegex.test(password) === false) {
+    res.render("auth/signup.hbs", {
+      error:
+        "The password should have at least 8 char, one uppercase, one lowercase and special char",
+    });
+    return;
+  }
+
+  // validación fields
+  if (username === "" || password === "") {
+    res.render("auth/signup.hbs", {
+      error: "All the fields should not be empty",
+    });
+    return;
+  }
+  try {
+    const uniqueUser = User.findOne({ username: username });
+    if (uniqueUser === null) {
+      res.render("auth/signup.hbs", {
+        error: "Username already exists",
+      });
+      return;
+    }
+
+    const salt = await bcrypt.genSalt(12);
+    const hashpassword = await bcrypt.hash(password, salt);
+
+    await User.create({
+      username: username,
+      password: hashpassword,
+    });
+    res.redirect("/auth/login");
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
